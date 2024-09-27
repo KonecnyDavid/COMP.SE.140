30e4eeedc25ba3174b1720d8bbcf4639eaab1b39
davidkonecny <david@konecny.eu>
2024-09-27 14:46:14 +0300

    feat: app complete

--- src/service-1/app.js
+++ src/service-1/app.js
@@ -4,11 +4,11 @@ const exec = util.promisify(require('child_process').exec);
 
 const app = express();
 
-app.get("/", async function(req, res) {
-    const {stdout: ip} = await exec('hostname -i');
-    const {stdout: proccesses} = await exec('ps -ax');
-    const {stdout: spaceAvailable} = await exec('df');
-    const {stdout: timeSinceBoot} = await exec('uptime');
+app.get("/", async function (req, res) {
+    const { stdout: ip } = await exec('hostname -i');
+    const { stdout: proccesses } = await exec('ps -ax');
+    const { stdout: spaceAvailable } = await exec('df');
+    const { stdout: timeSinceBoot } = await exec('uptime');
 
     const response = await fetch("http://service-2:8080/")
     const data = await response.json();
@@ -16,13 +16,14 @@ app.get("/", async function(req, res) {
     return res.send({
         service_2: data,
         service_1: {
-        ip: ip.trim(),
-        proccesses: proccesses.split("\n").map(p => p.trim()).filter(p => p.length !== 0), 
-        spaceAvailable: spaceAvailable.split("\n").map(p => p.trim()).filter(p => p.length !== 0),
-        timeSinceBoot: timeSinceBoot.trim()
-    }});
+            ip: ip.trim(),
+            proccesses: proccesses.split("\n").map(p => p.trim()).filter(p => p.length !== 0),
+            spaceAvailable: spaceAvailable.split("\n").map(p => p.trim()).filter(p => p.length !== 0),
+            timeSinceBoot: timeSinceBoot.trim()
+        }
+    });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
--- src/service-2/Program.cs
+++ src/service-2/Program.cs
@@ -1,23 +1,7 @@
 using service_2;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
 
 app.MapGet("/", () =>
     {
@@ -34,7 +18,6 @@ app.MapGet("/", () =>
             sinceBoot
         };
     })
-    .WithName("GetServiceInfo")
-    .WithOpenApi();
+    .WithName("GetServiceInfo");
 
 app.Run();
